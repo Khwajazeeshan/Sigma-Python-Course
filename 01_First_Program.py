5c09a63f16c99a34ff0ca84c087a2073416b4afb
Khawaja Zeeshan <123800392+Khwajazeeshan@users.noreply.github.com>
2024-08-22 11:47:09 +0500

    Add files via upload

---
+++ 01_First_Program.py
@@ -0,0 +1,14 @@
+""" how to Print
+using string
+using length function
+using For Loop """
+
+print("Hello World")
+print("my name is zeeshan\nI live in muzaffarabad")
+
+name = "Zeeshan"
+print(name)
+print(len(name))
+
+for character in name:
+    print(character)
